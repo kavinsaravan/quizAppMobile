b714297008072e8011592e2a19379fe87ba7aaf0
Kavin <kavinsaravan@gmail.com>
2024-08-30 18:13:30 -0700

    Initial Commit

---
+++ quizAppMobile/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  quizAppMobile
+//
+//  Created by Kavin Saravanan on 8/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
